1be8776988ccc0f67225eff20e7fcd0f2ab850a6
lambotik <lambotik1988@gmail.com>
2023-06-28 16:09:51 +0300

    Update: mobile.py, mobile_logic.py Added: mobile.py, mobile_logic.py
    Signed-off-by: lambotik <lambotik1988@gmail.com>

---
+++ mobile_operator/mobile.py
@@ -0,0 +1,17 @@
+from mobile_operator.mobile_logic import SQLMobile
+
+
+class MOBILE:
+    def mobile_balance_check(self):
+        SQLMobile.create_table_mobile_users()
+        SQLMobile.create_table_mobile_tariff()
+        SQLMobile.insert_user(('User1', 10000, 2, 'Yes'))
+        SQLMobile.insert_user(('User2', 10000, 3, 'Yes'))
+        SQLMobile.insert_user(('User3', 10000, 1, 'Yes'))
+        SQLMobile.insert_tariff(('Standard', 500))
+        SQLMobile.insert_tariff(('VIP', 1000))
+        SQLMobile.insert_tariff(('Premium', 1500))
+
+
+start = MOBILE()
+start.mobile_balance_check()
